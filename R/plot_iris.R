7ac8ee79dcfcd2dbd373e759e4b721d952d81796
yutong <yutongw@umich.edu>
2019-01-26 16:32:00 -0500

    Made the "import" work.

--- R/plot_iris.R
+++ R/plot_iris.R
@@ -4,8 +4,11 @@
 #' @param y the column-id of y-dim
 #' @examples
 #' plot_iris(1,2)
+#' @import ggplot2
 
 plot_iris <- function(x,y) {
-  plot(iris[,c(x,y)])
+  x_name <- colnames(iris)[x]
+  y_name <- colnames(iris)[y]
+  ggplot(iris,aes_string(x_name,y_name)) + geom_point(aes(color=Species))
 }
 
