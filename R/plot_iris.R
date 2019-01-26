f1b6360073a6c28f8ae05c41abc52b0e8241bb26
yutong <yutongw@umich.edu>
2019-01-26 16:13:26 -0500

    Documentation added.

--- R/plot_iris.R
+++ R/plot_iris.R
@@ -1,3 +1,11 @@
-plot_iris <- function() {
-  plot(iris[,1:2])
+#' Plot a scatter plot of the iris data set
+#'
+#' @param x the column-id of x-dim
+#' @param y the column-id of y-dim
+#' @examples
+#' plot_iris(1,2)
+
+plot_iris <- function(x,y) {
+  plot(iris[,c(x,y)])
 }
+
