3cde75b4a52a3f461a462c219147e1e5695c405a
yutong <yutongw@umich.edu>
2019-01-26 19:17:57 -0500

    Added a vignette.

---
+++ R/plot_df.R
@@ -0,0 +1,17 @@
+#' Plot a scatterplot of the input dataset
+#'
+#' @param df the data frame
+#' @param x the column-id of x-dim
+#' @param y the column-id of y-dim
+#' @param color the column-id of
+#' @examples
+#' plot_df(df = iris,x = 1,y = 2,color_use = 5)
+#' @importFrom ggplot2 ggplot geom_point aes_string
+#' @export
+
+plot_df <- function(df,x,y,color_use){
+  x_name <- colnames(iris)[x]
+  y_name <- colnames(iris)[y]
+  color_name <- colnames(iris)[color_use]
+  ggplot(df,aes_string(x_name,y_name)) + geom_point(aes_string(color=color_name))
+}
